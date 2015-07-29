6df53c566b0e8e3c3349fb66b5d32baf37d58aa0
Andy Choi <andy.dh.choi@gmail.com>
2015-07-29 15:46:25 -0500

    layout test scenarios

--- test/test.js
+++ test/test.js
@@ -1,5 +1,37 @@
-// TODO:
-// 1. Test with no animation
-//   - basic functionalities
-// 2. Test with animation
-//   - different settings
+// TODO: Create test file for following cases
+//
+// 1. Behavioral Test Cases
+//   a. when there is no selected child
+//     - adding from either end
+//       > should not animate
+//     - adding from middle
+//       > should animate if set so
+//   b. when there is only one selected child
+//     - adding
+//       - element adjacent to selected list
+//         > should not animate
+//       - element not adjacent to selected list
+//         > should animate if set so
+//     - removing
+//       - element
+//         > should not animate
+//   c. when there is more than one selected child
+//     - adding
+//       - element adjacent to selected list
+//         > should not animate
+//       - element not adjacent to selected list
+//         > should animate if set so
+//     - removing
+//       - element adjacent to non-selected list
+//         > should not animate
+//       - element not adjacent to non-selected list
+//			> should animate if set so
+//
+// 2. Settings Test Cases
+//   a. childSelector
+//   b. selectedChildClass
+//   c. showAnimation
+//   d. animation
+//   e. animationSpeed
+//   f. reverse
+//   g. callback
